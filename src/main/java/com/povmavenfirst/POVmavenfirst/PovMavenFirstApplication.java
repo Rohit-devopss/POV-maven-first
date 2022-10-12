7d23eb1585ed316c186ea13beae7b5564256700f
Rohit Shinde <rohit.shinde@nobrainsolutions.com>
2022-10-12 16:31:04 +0530

    pov-commit

---
+++ src/main/java/com/povmavenfirst/POVmavenfirst/PovMavenFirstApplication.java
@@ -0,0 +1,13 @@
+package com.povmavenfirst.POVmavenfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PovMavenFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PovMavenFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/povmavenfirst/POVmavenfirst/PovMavenFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.povmavenfirst.POVmavenfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PovMavenFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
